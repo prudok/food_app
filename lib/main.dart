93362081e0eb526b13f85c9ed209115ca2d55c03
rel1nce <romanprudchen@gmail.com>
2023-08-01 01:26:42 +0300

    Revert "style: add error handling function in main.dart & changes in gitignore"
    This reverts commit c4d7b8263cbaffdc18ac4b48d798b1a4f7a56334.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/app.dart';
 import 'package:food_app/core/injection.dart';
@@ -9,21 +6,10 @@ import 'package:food_app/features/notifications/data/datasource/firebase_api_imp
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  await runZonedGuarded(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
-      await Firebase.initializeApp();
-      await initializeDateFormatting();
-      getIt.setDependencies();
-      await getIt.get<FirebaseAPIImpl>().initNotifications();
-      runApp(const FoodApp());
-    },
-    (error, stack) {
-      if (kDebugMode) {
-        print(' ---- Something went wrong. ---- ');
-        print('Error: $error');
-        print('StackTrace: \n$stack');
-      }
-    },
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await initializeDateFormatting();
+  getIt.setDependencies();
+  await getIt.get<FirebaseAPIImpl>().initNotifications();
+  runApp(const FoodApp());
 }
