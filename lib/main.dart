76afd2908c173a94f031d9350fccf9de696c1398
rel1nce <romanprudchen@gmail.com>
2023-08-01 01:30:51 +0300

    style: add zone in main.dart & rules in gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/app.dart';
 import 'package:food_app/core/injection.dart';
@@ -6,10 +9,21 @@ import 'package:food_app/features/notifications/data/datasource/firebase_api_imp
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await initializeDateFormatting();
-  getIt.setDependencies();
-  await getIt.get<FirebaseAPIImpl>().initNotifications();
-  runApp(const FoodApp());
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      await initializeDateFormatting();
+      getIt.setDependencies();
+      await getIt.get<FirebaseAPIImpl>().initNotifications();
+      runApp(const FoodApp());
+    },
+    (error, stack) {
+      if (kDebugMode) {
+        print(' ---- Something went wrong. ---- ');
+        print('Error: $error');
+        print('StackTrace: \n$stack');
+      }
+    },
+  );
 }
